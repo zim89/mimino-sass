1c576288c19c84e9cf9addf4e1db9f32aa38366f
zim89 <121241841+zim89@users.noreply.github.com>
2023-03-21 13:49:28 +0000

    Deploying to gh-pages from @ zim89/mimino-sass@f5534f034012e46bfc0b43abb9482acba3a8059a 🚀

--- hotel.b21fb8f2.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".hotel-swiper",{slidesPerView:1.5,spaceBetween:20,breakpoints:{768:{spaceBetween:32}},navigation:{nextEl:".btn--right",prevEl:".btn--left"}});
-//# sourceMappingURL=hotel.b21fb8f2.js.map
--- hotel.c707eb75.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".hotel-swiper",{slidesPerView:1.5,spaceBetween:20,breakpoints:{768:{spaceBetween:32}},navigation:{nextEl:".btn--right",prevEl:".btn--left"}});
-//# sourceMappingURL=hotel.c707eb75.js.map
